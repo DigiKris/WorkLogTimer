279fa8b610385f1e8cf1ea4c8a816b2055d7b66a
DigiKris <digikrisu@gmail.com>
2021-06-18 15:13:45 +0300

    Grouped code

--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,7 @@ namespace WorkLogTimer
         {
             InitializeComponent();
         }
-
+        #region Add to combobox
         private void Form1_Load(object sender, EventArgs e)
         {
             this.buttonWorkStop.Enabled = false;
@@ -37,51 +37,47 @@ namespace WorkLogTimer
             }
 
             this.comboBoxWorkHours.SelectedIndex = 00;
-
             this.comboBoxWorkMinutes.SelectedIndex = 00;
-
             this.comboBoxWorkSeconds.SelectedIndex = 00;
 
         }
+        #endregion
 
+        #region Buttons
         private void buttonWorkStart_Click(object sender, EventArgs e)
         {
             this.buttonWorkStart.Enabled = false;
-
             this.buttonWorkStop.Enabled = true;
 
 
 
             int hours = int.Parse(this.comboBoxWorkHours.SelectedItem.ToString());
-
             int minutes = int.Parse(this.comboBoxWorkMinutes.SelectedItem.ToString());
-
             int seconds = int.Parse(this.comboBoxWorkSeconds.SelectedItem.ToString());
 
             totalSeconds = (minutes * 60) + (hours * 3600) + seconds;
 
             this.timerWork.Enabled = true;
         }
+        
 
         private void buttonWorkStop_Click(object sender, EventArgs e)
         {
             totalSeconds = 0;
 
             this.buttonWorkStop.Enabled = false;
-
             this.buttonWorkStart.Enabled = true;
-
             this.timerWork.Enabled = false;
 
             int hours = totalSeconds / 3600;
-
             int minutes = totalSeconds / 60;
-
             int seconds = totalSeconds - (minutes * 60);
 
             this.labelWorkCountdown.Text = hours.ToString().PadLeft(2, '0') + ":" + minutes.ToString().PadLeft(2, '0') + ":" + seconds.ToString().PadLeft(2, '0');
         }
+        #endregion
 
+        #region Timer
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (totalSeconds > 0)
@@ -114,5 +110,6 @@ namespace WorkLogTimer
 
             }
         }
+        #endregion
     }
 }
